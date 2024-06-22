94ba495adf189413f32c81f907daddde3512643a
Nayon-s <nayonkarmoker3@gmail.com>
2024-06-22 17:29:02 +0600

    making few changes

--- backend/database.js
+++ backend/database.js
@@ -1,8 +1,26 @@
+// const mongoose = require('mongoose');
+
+// async function connection() {
+//   await mongoose.connect('mongodb+srv://nayon49:smith49@cluster0.yl2sw9b.mongodb.net/courses?retryWrites=true&w=majority&appName=Cluster0'); 
+//     console.log("database connected")
+// }
+
+// module.exports=connection
 const mongoose = require('mongoose');
 
 async function connection() {
-  await mongoose.connect('mongodb+srv://nayon49:smith49@cluster0.yl2sw9b.mongodb.net/courses?retryWrites=true&w=majority&appName=Cluster0'); 
-    console.log("database connected")
+  try {
+    await mongoose.connect('mongodb+srv://nayon49:smith49@cluster0.yl2sw9b.mongodb.net/courses?retryWrites=true&w=majority&ssl=true', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Database connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    if (err.cause) {
+      console.error("Cause:", err.cause);
+    }
+  }
 }
 
-module.exports=connection
+module.exports = connection;
